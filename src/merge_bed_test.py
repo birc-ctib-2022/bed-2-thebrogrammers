8bca1bd9fbbae16e32e177bb370503c823accc76
PeterKADam <peter.konrad.alling@gmail.com>
2022-11-03 11:56:25 +0100

    merge test

--- src/merge_bed_test.py
+++ src/merge_bed_test.py
@@ -1,6 +1,14 @@
 # This directory will be checked with pytest. It will examine
 # all files that start with test_*.py and run all functions with
 # names that start with test_
+import os
+import filecmp
+file = "data/input-sorted.bed"
+out = "data/testoutmerge.bed"
+expected = "data/input-merged.bed"
 
-def test_1984():
-    
+
+def test_merge():
+    os.system("python src/merge_bed.py " + file +
+              " " + file + " -o" + out)
+    assert filecmp.cmp(out, expected)

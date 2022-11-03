5151df06e8a6365f96cb901b4f8224f828ffc74c
PeterKADam <peter.konrad.alling@gmail.com>
2022-11-03 10:22:09 +0100

    this feels illegal

--- src/sort_bed.py
+++ src/sort_bed.py
@@ -15,7 +15,7 @@ def sort_file(table: Table) -> None:
         # Here we iterate through all the chromosomes in the file.
         # You need to sort `features` with respect to chrom_start
         # and then updatte the table
-        features.sort()
+        features.sort() #sorted
         table[chrom] = features  # features should be sorted here
 
 
